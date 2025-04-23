d54c4465170787529cfe1b2f823789357088f449
Abhishek Choudhary <142403448+AbhishekChoudharyy@users.noreply.github.com>
2025-04-23 16:48:42 +0530

    3d model testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import React from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = "https://unpkg.com/@splinetool/viewer@1.9.82/build/spline-viewer.js";
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div style={{
-      height: "100%",
-      width: "100%",
-      backgroundColor: "black",
-      margin: 0,
-      padding: 0
-    }}>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "black",
+        margin: 0,
+        padding: 0,
+      }}
+    >
+      <spline-viewer
+        loading-anim-type="spinner-small-dark"
+        url="https://prod.spline.design/dBgM7heoEmjDSdB7/scene.splinecode"
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "block",
+          border: "none",
+        }}
+      ></spline-viewer>
     </div>
   );
 };
 
 export default App;
-
